d45de1c25422eb24129d8997f1da3b5061145101
Ronald Hong <rhong01@wr-130-64-223-55.medford.tufts.edu>
2013-04-10 01:34:57 -0400

    scraped URLs

---
+++ scraping/requestsTest.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url = 'http://www.iheart.com/find/'
+payload = {'zip':'98102'}
+r = requests.post(url, payload)
+
+if(r.status_code == requests.codes.ok):
+	soup = BeautifulSoup(r.content)
+	print(soup.prettify())
+
+	addresses = soup.findAll('option', attrs = {})
+	results = []
+	with open('some.csv', 'wb') as f:
+		writer = csv.writer(f)
+		for a in addresses:
+			writer.writerow([a.text, a.get('value')])
