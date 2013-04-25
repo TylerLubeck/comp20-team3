5262f05cd3e78b9b1d8cde8f8edd4d1838acb333
rhong01 <ronniejhong@gmail.com>
2013-04-25 01:26:06 -0400

    got radio stations

--- scraping/current.py
+++ scraping/current.py
@@ -5,17 +5,20 @@ import json
 base_url = 'http://www.iheart.com'
 with open('scraped_urls.json', 'r') as infile:
 	urls = json.load(infile)
-
-key = 'Washington'
-print("scraping url: " + base_url + urls[key])
-page = urllib2.urlopen(base_url + urls[key])
-soup = BeautifulSoup(page)
-
-stations = soup.findAll('p', attrs = {'class':'stnTitle'})
-
 results = {}
-for itr in range(0,len(stations)-1):
-	t = stations[itr].find('a').text
-	l = stations[itr].find('a').get('href')
-	print(t)
-	print(l)
+
+for key in urls:
+	print("scraping url: " + base_url + urls[key] + " as " + key)
+	page = urllib2.urlopen(base_url + urls[key])
+	soup = BeautifulSoup(page)
+
+	stations = soup.findAll('p', attrs = {'class':'stnTitle'})
+
+	allLinks = {}
+	for itr in range(0,len(stations)-1):
+		title = stations[itr].find('a').text
+		link = stations[itr].find('a').get('href')
+		allLinks[title] = link
+	results[key] = allLinks
+with open('radio_stations.json', 'w') as outfile:
+	json.dump(results, outfile)
