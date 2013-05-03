7e814bd91d66c9fd78f067763e959836c2150bf6
Joe <joseph.canuel@tufts.edu>
2013-05-03 04:38:20 -0400

    working on graphs

--- front_end/bootstrap_files/js/googlegraphs.js
+++ front_end/bootstrap_files/js/googlegraphs.js
@@ -9,15 +9,14 @@
       function drawChart() {
           $.get('http://afternoon-anchorage-3983.herokuapp.com/station_info?station=' + localStorage.station0, function(data) {
           	console.log(data);
-          	var x = data;
           	var count = new Array();
           	for(i = 1; i < 6; i++)
           		count[i] = 0;
-          	});
-          	for(i = 0; i < x.length; i++){
+
+          	for(i = 0; i < data.length; i++){
           		console.log(i);
           	}
-
+          	});
       
         var data = google.visualization.arrayToDataTable([
           ['Year', 'Sales', 'Expenses'],
