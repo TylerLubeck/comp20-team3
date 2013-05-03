e45170d09072281d274aabd5150f75c75e290001
Tyler Lubeck <tyler@tylerlubeck.com>
2013-05-02 22:21:20 -0700

    added debug message

--- scripts/login.js
+++ scripts/login.js
@@ -31,6 +31,7 @@ function login() {
     }
     serverName = server + '/login.json';
     $.get(serverName, {'UN':UN, 'PW':PW}, function(data) {
+        console.log(data);
         if (data != 'false') {
             localStorage.userName = data;
             window.location.href = 'your_music.html';
