19e8620de9f63c1cbe9a770ac35e5130fbd72d0c
Tyler Lubeck <tyler@tylerlubeck.com>
2013-05-02 22:07:48 -0700

    debug message

--- scripts/stationRanks.js
+++ scripts/stationRanks.js
@@ -1,6 +1,7 @@
 var server = 'http://afternoon-anchorage-3983.herokuapp.com';
 function getRank(stationName, i) {
     var tdRankI = $('#tdRank' + i);    
+    console.log(tdRankI);
     serverName = server + '/getRanking';
     $.get(serverName, {'station':stationName}, function(data) {
         tdRankI.css("background-color", "red");
