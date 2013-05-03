e58cdefcf55c50240d03565313a14310eca3a84e
Joe <joseph.canuel@tufts.edu>
2013-05-03 04:29:39 -0400

    working on graphs

--- front_end/bootstrap_files/js/googlegraphs.js
+++ front_end/bootstrap_files/js/googlegraphs.js
@@ -10,6 +10,7 @@
           $.get('http://afternoon-anchorage-3983.herokuapp.com/station_info?station=' + localStorage.station0, function(data) {
           	console.log(data);
           	var x = data;
+          	localStorage.testing = data;
           	var count = new Array();
           	for(i = 1; i < 6; i++)
           		count[i] = 0;
