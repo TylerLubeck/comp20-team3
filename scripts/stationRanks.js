a23ac6107607bd2c3027ba881d16c8d830bf20f8
Tyler Lubeck <tyler@tylerlubeck.com>
2013-05-02 21:47:03 -0700

    adding ranking

--- scripts/stationRanks.js
+++ scripts/stationRanks.js
@@ -1,13 +1,16 @@
 var server = 'http://afternoon-anchorage-3983.herokuapp.com';
-function getRank(stationName) {
-    
+function getRank(stationName, i) {
+    var tdRankI = $('#tdRank' + i);    
     serverName = server + '/getRanking';
     $.get(serverName, {'station':stationName}, function(data) {
-        if(data == undefined) {
-            alert('Be the first to rank this station!');
-        } else {
-            alert('The station rank is ' + data);
-        }
-
+        alert('The station rank is ' + data);
+        tdRankI.innerHTML = data;
     });
 }
+/*
+function addRankButton(stationName, i) {
+    $('#tdRankButton' + i).click(function() {
+        $.
+    });
+}
+*/
